ea9e2165097d5c5b63922be5a49d5f1c1deabcd2
Ronan Morais <ronan86@gmail.com>
2023-05-11 16:19:25 -0300

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
-import styles from './App.module.css';
-
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        
-      </header>
+    <div class='bg-orange-400 flex'>
+      teste
     </div>
   );
 };
