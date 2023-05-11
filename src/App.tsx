682654e05323934827dcdf0423e2aab6c06f48c3
Ronan Morais <ronan86@gmail.com>
2023-05-10 22:40:09 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import type { Component } from 'solid-js';
+
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        
+      </header>
+    </div>
+  );
+};
+
+export default App;
